81b9797508108a9433343f41c6f5a58a4392c422
Sufyankhan2004 <crystalsmooth3@gmail.com>
2026-07-04 14:08:18 +0500

    Fix weather icon updates

--- Weather.js
+++ Weather.js
@@ -5,6 +5,7 @@
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=`;
         const searchBox = document.querySelector('.search input');
         const searchBtn = document.querySelector('.search button');
+        const weatherIcon = document.querySelector('.weather-icon');
 
         async function checkWeather(city) {
             const response = await fetch(apiUrl + city);
@@ -20,6 +21,22 @@
             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
             document.querySelector(".humidity").innerHTML = (data.main.humidity) + "%";
             document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+            const weatherType = data.weather[0].main.toLowerCase();
+
+            if (weatherType === "clouds") {
+                weatherIcon.src = "Images/clouds.png";
+            } else if (weatherType === "drizzle") {
+                weatherIcon.src = "Images/drizzle.png";
+            } else if (weatherType === "rain") {
+                weatherIcon.src = "Images/rain.png";
+            } else if (weatherType === "mist") {
+                weatherIcon.src = "Images/mist.png";
+            } else if (weatherType === "snow") {
+                weatherIcon.src = "Images/snow.png";
+            } else {
+                weatherIcon.src = "Images/clear.png";
+            }
 
         }
 
